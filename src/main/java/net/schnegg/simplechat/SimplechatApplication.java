3722b3cb8f70f18b5122aaf2fd5560dbf53e5887
Frank Rittinger <frank@schnegg.net>
2024-11-10 11:08:59 +0100

    initial boot app

---
+++ src/main/java/net/schnegg/simplechat/SimplechatApplication.java
@@ -0,0 +1,13 @@
+package net.schnegg.simplechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplechatApplication.class, args);
+	}
+
+}
